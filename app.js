51b8514f80c42d432d1a7d4b613cde4c112a5986
mohamedali654321 <mmmmali654321@gmail.com>
2020-07-09 20:45:18 +0300

    first commit

--- app.js
+++ app.js
@@ -1,59 +1,91 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const mongoose=require('mongoose');
-const db=require('./models/db');
-const expValidator=require('express-validator');
-const session=require('express-session');
-const flash =require('connect-flash');
-const passport=require('passport');
-const ehbs=require('express-handlebars');
+#!/usr/bin/env node
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+/**
+ * Module dependencies.
+ */
 
-var app = express();
-require('./config/passport');
+var app = require('./apps');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+var debug = require('debug')('clothing-shop:server');
+var http = require('http');
 
+/**
+ * Get port from environment and store in Express.
+ */
 
-app.set('view engine', 'hbs');
+var port = normalizePort(process.env.PORT || '5000');
+app.set('port', port);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(session({
-  secret:'restaurant_?@!' ,
-  saveUninitialized :false ,
-  resave :true
-}));
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
+/**
+ * Create HTTP server.
+ */
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+var server = http.createServer(app);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+/**
+ * Listen on provided port, on all network interfaces.
+ */
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+server.listen(port,()=>{console.log('server is running on port 5000 ');});
+server.on('error', onError);
+server.on('listening', onListening);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+/**
+ * Normalize a port into a number, string, or false.
+ */
 
-module.exports = app;
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
